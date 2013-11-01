55e41cdab6afb3c81d0e0522b135a8fb4a9f8a1d
friendica <info@friendica.com>
2013-11-01 00:30:03 -0700

    updates

--- data/configuration.js
+++ data/configuration.js
@@ -9,6 +9,8 @@ self.port.on("done", function(data) {
 // add callback for form submission event
 document.getElementById("submit").addEventListener("click", function() {
   var network_url = document.forms[0].elements["network_url"].value;
+	if(network_url.search('/rpost') < 0)
+		network_url = network_url + '/rpost';	
   self.port.emit("configure", {"network_url":network_url});
 
   return false;
